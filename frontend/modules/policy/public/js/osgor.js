173dffbb2edab7d961d247e9a51dce28dd42b4fe
webcoderphp <asadbekdev007@gmail.com>
2024-07-16 14:54:43 +0500

    osgor form modified

--- frontend/modules/policy/public/js/osgor.js
+++ frontend/modules/policy/public/js/osgor.js
@@ -10,115 +10,115 @@ $(function() {
     hundredYearsAgo = currentDate.setFullYear(currentDate.getFullYear()-100);
 
 
-    if ($('body').find('#policyosgo-start_date')) {
-        var startDate = $('#policyosgo-start_date');
-        var endDate = $('#policyosgo-end_date');
-
-        startDate.datepicker({
-            autoHide: true,
-            startDate: new Date((new Date()).valueOf() + 1000*3600*24*0),
-            minDate: new Date((new Date()).valueOf() + 1000*3600*24*0),
-            endDate: new Date(new Date().setDate(new Date().getDate() + 30)),
-            maxDate: new Date(new Date().setDate(new Date().getDate() + 30)),
-            format: 'dd.mm.yyyy'
-        });
-        endDate.datepicker({
-            autoHide: true,
-            format: 'dd.mm.yyyy',
-            date: startDate.datepicker('getDate'),
-            startDate: startDate.datepicker('getDate')
-        });
-
-        startDate.on('changeDate', function () {
-            endDate.datepicker('setDate', startDate.datepicker('getDate'));
-            endDate.datepicker('setStartDate', startDate.datepicker('getDate'));
-            endDate.datepicker('setMinDate', startDate.datepicker('getDate'));
-            endDate.show();
-            startDate.hide();
-        });
-
-        var from;
-        var days;
-        var to;
-
-        function addDays(date, days) {
-            days--;
-            // console.log("date1");
-            // console.log(date);
-            var date_split = date.split(".");
-            // console.log("date_split");
-            // console.log(date_split);
-            var new_date = date_split[2]+"-"+date_split[1]+"-"+date_split[0];
-            // console.log("new_date");
-            // console.log(new_date);
-            var result = new Date(new_date);
-            // console.log("result1");
-            // console.log(result);
-            result.setDate(result.getDate() + Number(days));
-            // console.log("result2");
-            // console.log(result);
-            var year_curr = result.getFullYear();
-            var month_curr = result.getMonth()<9?'0'+(result.getMonth()+1):(result.getMonth()+1);
-            var date_cur = result.getDate()<10?'0'+result.getDate():result.getDate();
-            var full_date = date_cur+'.'+month_curr+'.'+year_curr;
-            // console.log("Full_date");
-            // console.log(full_date);
-            return full_date;
-        }
-
-        function subtractDays(strdate1, strdate2){
-            var date_split = strdate1.split(".");
-            var new_strdate1 = date_split[2]+"-"+date_split[1]+"-"+date_split[0];
-            const date1 = new Date(new_strdate1);
-            date_split = strdate2.split(".");
-            var new_strdate2 = date_split[2]+"-"+date_split[1]+"-"+date_split[0];
-            const date2 = new Date(new_strdate2);
-            const diffTime = Math.abs(date2 - date1);
-            const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-            return diffDays+1;
-        }
-
-        $(document).on('change', "#policyosgo-start_date", function (e) {
-            from = e.target.value;
-            aggregated('FROM');
-        })
-        $(document).on('change', "#policyosgo-days", function (e) {
-            days = e.target.value;
-            aggregated('DAYS');
-        })
-        $(document).on('change', "#policyosgo-end_date", function (e) {
-            to = e.target.value;
-            aggregated('TO');
-        })
-
-        function aggregated(TARGET){
-            switch(TARGET){
-                case 'FROM':
-                    if(!days){
-                        days=365;
-                    }
-                    $('#policyosgo-days').val(days);
-                    $('#policyosgo-end_date').attr('min',from)
-                    $('#policyosgo-end_date').val(addDays(from, days))
-                    break;
-                case 'TO':
-                    if(!from)
-                        break;
-                    days = subtractDays(from, to);
-                    $('#policyosgo-end_date').attr('min',from)
-                    $("#policyosgo-days").val(days);
-                    break;
-                case 'DAYS':
-                    if(!from)
-                        break;
-                    // console.log(from,days)
-                    $('#policyosgo-end_date').attr('min',from)
-                    $('#policyosgo-end_date').val(addDays(from, days))
-                    break;
-            }
-        }
-
-    }
+    // if ($('body').find('#policyosgo-start_date')) {
+    //     var startDate = $('#policyosgo-start_date');
+    //     var endDate = $('#policyosgo-end_date');
+    //
+    //     startDate.datepicker({
+    //         autoHide: true,
+    //         startDate: new Date((new Date()).valueOf() + 1000*3600*24*0),
+    //         minDate: new Date((new Date()).valueOf() + 1000*3600*24*0),
+    //         endDate: new Date(new Date().setDate(new Date().getDate() + 30)),
+    //         maxDate: new Date(new Date().setDate(new Date().getDate() + 30)),
+    //         format: 'dd.mm.yyyy'
+    //     });
+    //     endDate.datepicker({
+    //         autoHide: true,
+    //         format: 'dd.mm.yyyy',
+    //         date: startDate.datepicker('getDate'),
+    //         startDate: startDate.datepicker('getDate')
+    //     });
+    //
+    //     startDate.on('changeDate', function () {
+    //         endDate.datepicker('setDate', startDate.datepicker('getDate'));
+    //         endDate.datepicker('setStartDate', startDate.datepicker('getDate'));
+    //         endDate.datepicker('setMinDate', startDate.datepicker('getDate'));
+    //         endDate.show();
+    //         startDate.hide();
+    //     });
+    //
+    //     var from;
+    //     var days;
+    //     var to;
+    //
+    //     function addDays(date, days) {
+    //         days--;
+    //         // console.log("date1");
+    //         // console.log(date);
+    //         var date_split = date.split(".");
+    //         // console.log("date_split");
+    //         // console.log(date_split);
+    //         var new_date = date_split[2]+"-"+date_split[1]+"-"+date_split[0];
+    //         // console.log("new_date");
+    //         // console.log(new_date);
+    //         var result = new Date(new_date);
+    //         // console.log("result1");
+    //         // console.log(result);
+    //         result.setDate(result.getDate() + Number(days));
+    //         // console.log("result2");
+    //         // console.log(result);
+    //         var year_curr = result.getFullYear();
+    //         var month_curr = result.getMonth()<9?'0'+(result.getMonth()+1):(result.getMonth()+1);
+    //         var date_cur = result.getDate()<10?'0'+result.getDate():result.getDate();
+    //         var full_date = date_cur+'.'+month_curr+'.'+year_curr;
+    //         // console.log("Full_date");
+    //         // console.log(full_date);
+    //         return full_date;
+    //     }
+    //
+    //     function subtractDays(strdate1, strdate2){
+    //         var date_split = strdate1.split(".");
+    //         var new_strdate1 = date_split[2]+"-"+date_split[1]+"-"+date_split[0];
+    //         const date1 = new Date(new_strdate1);
+    //         date_split = strdate2.split(".");
+    //         var new_strdate2 = date_split[2]+"-"+date_split[1]+"-"+date_split[0];
+    //         const date2 = new Date(new_strdate2);
+    //         const diffTime = Math.abs(date2 - date1);
+    //         const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    //         return diffDays+1;
+    //     }
+    //
+    //     $(document).on('change', "#policyosgo-start_date", function (e) {
+    //         from = e.target.value;
+    //         aggregated('FROM');
+    //     })
+    //     $(document).on('change', "#policyosgo-days", function (e) {
+    //         days = e.target.value;
+    //         aggregated('DAYS');
+    //     })
+    //     $(document).on('change', "#policyosgo-end_date", function (e) {
+    //         to = e.target.value;
+    //         aggregated('TO');
+    //     })
+    //
+    //     function aggregated(TARGET){
+    //         switch(TARGET){
+    //             case 'FROM':
+    //                 if(!days){
+    //                     days=365;
+    //                 }
+    //                 $('#policyosgo-days').val(days);
+    //                 $('#policyosgo-end_date').attr('min',from)
+    //                 $('#policyosgo-end_date').val(addDays(from, days))
+    //                 break;
+    //             case 'TO':
+    //                 if(!from)
+    //                     break;
+    //                 days = subtractDays(from, to);
+    //                 $('#policyosgo-end_date').attr('min',from)
+    //                 $("#policyosgo-days").val(days);
+    //                 break;
+    //             case 'DAYS':
+    //                 if(!from)
+    //                     break;
+    //                 // console.log(from,days)
+    //                 $('#policyosgo-end_date').attr('min',from)
+    //                 $('#policyosgo-end_date').val(addDays(from, days))
+    //                 break;
+    //         }
+    //     }
+    //
+    // }
 
     function _getCalc() {
 
