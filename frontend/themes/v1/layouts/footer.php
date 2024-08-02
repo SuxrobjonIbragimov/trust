774130c75c6bc2227607cc8bae3153b865d413c6
Shohdevuz <shohdevuz@gmailc.om>
2024-08-02 19:37:10 +0500

    footer phone and author info modified #2

--- frontend/themes/v1/layouts/footer.php
+++ frontend/themes/v1/layouts/footer.php
@@ -8,6 +8,7 @@ use yii\helpers\Html;
 use yii\helpers\Url;
 
 $phone = Settings::getValueByKey(Settings::KEY_MAIN_PHONE);
+$phone2 = '+998 71 255 66 00';
 $email = Settings::getValueByKey(Settings::KEY_MAIN_EMAIL);
 $location = HtmlParts::getItemByKey(HtmlParts::KEY_MAIN_LOCATION);
 ?>
@@ -16,21 +17,21 @@ $location = HtmlParts::getItemByKey(HtmlParts::KEY_MAIN_LOCATION);
 <!--========================================================================-->
 <!-- |FOOTER|-->
 <!--========================================================================-->
-<footer class="footer-area bg-dark text-light section-gap mt-5 pt-5 pb-5" id="contacts">
+<footer class="footer-area bg-dark text-light section-gap mt-5 pt-5 pb-3" id="contacts">
     <div class="container">
         <div class="row">
             <div class="col-lg-5 col-md-6 col-sm-6">
                 <div class="single-footer-widget">
                     <h5><?= Yii::t('footer', 'Наши контакты') ?></h5>
-                    <p><?= Yii::t('footer','Телефон'); ?>: <a href="tel:+<?= clear_phone_full($phone); ?>" class="contacts"><?= $phone; ?></a></p>
+                    <p class="contacts mt-3"><?= Yii::t('footer','Телефон'); ?>:
+                        <br>
+                        <a href="tel:+<?= clear_phone_full($phone); ?>" class="contacts"><?= $phone; ?></a>
+                        <br>
+                        <a href="tel:+<?= clear_phone_full($phone2); ?>" class="contacts"><?= $phone2; ?></a>
+                    </p>
                     <p><?= Yii::t('footer','Почта'); ?>: <a href="mailto:<?= $email; ?>" class="contacts"><?= $email; ?></a></p>
                     <p><?= Yii::t('footer','Адрес'); ?>: <?= !empty($location) ? $location->body : null ?></p>
-                    <p class="footer-text"><?= Yii::t('footer','Copyright ©'); ?>
-                        <script>
-                            document.write(new Date().getFullYear());
-                        </script>
-                        <?= Yii::t('footer','Все права защищены'); ?>
-                    </p>
+
                 </div>
             </div>
             <div class="col-lg-5 col-md-6 col-sm-6">
@@ -44,6 +45,26 @@ $location = HtmlParts::getItemByKey(HtmlParts::KEY_MAIN_LOCATION);
                 <div class="single-footer-widget">
                     <h5 class="text-center text-sm-start mb-2"><?= Yii::t('footer','Мы в соц сетях'); ?></h5>
                     <?= SocialNetworksWidget::widget(['has_image' => false,'tag_class' => 'footer-social d-flex align-items-center justify-content-sm-between justify-content-around']) ?>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-3">
+            <div class="col-lg-6 col-md-6 col-sm-6">
+                <div class="single-footer-widget">
+                    <p class="footer-text"><?= Yii::t('footer','Copyright ©'); ?>
+                        <script>
+                            document.write(new Date().getFullYear());
+                        </script>
+                        <?= Yii::t('footer','Все права защищены'); ?>
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6">
+                <div class="d-flex justify-content-end w-100">
+                    <a href="http://triger.uz/"
+                       class="primary-hover transition fs-7 fw-bold text-white"
+                       rel="nofollow"
+                       target="_blank"><?= Yii::t('footer', 'Developed by {companyName}', ['companyName' => 'TRIGGER']) ?></a>
                 </div>
             </div>
         </div>
